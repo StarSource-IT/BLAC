13a78c7197ba8f4ce740d50c56428d0f5da8d47e
Drob11 <115478064+Drob11@users.noreply.github.com>
2026-09-10 16:11:03 -0400

    Update header.tsx
    Added join newsletter button with link to newsletter URL

--- app/routes/layout/header.tsx
+++ app/routes/layout/header.tsx
@@ -58,6 +58,10 @@ const menu: Menu[] = [
 		title: 'Contact us',
 		to: contactLink,
 	},
+	{
+		title: 'Join Newsletter',
+		to: 'https://cincyblac.us4.list-manage.com/subscribe?u=25b60bf7591a2072eed249df0&id=ba5aa3e5df',
+	},
 ]
 
 const buttons = {
